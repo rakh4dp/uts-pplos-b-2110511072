2cd41ecb68473b174bf8a40d13a17d132c1b48cf
rakha <dwiprayogarakha@gmail.com>
2026-04-28 17:55:14 +0700

    feat: implement database connection and user model

---
+++ services/auth-service/models/user.js
@@ -0,0 +1,17 @@
+const db = require('../config/db');
+
+const User = {
+    findByGoogleId: (googleId, callback) => {
+        db.query('SELECT * FROM users WHERE google_id = ?', [googleId], callback);
+    },
+
+    findByEmail: (email, callback) => {
+        db.query('SELECT * FROM users WHERE email = ?', [email], callback);
+    },
+
+    create: (userData, callback) => {
+        db.query('INSERT INTO users SET ?', userData, callback);
+    }
+};
+
+module.exports = User;
