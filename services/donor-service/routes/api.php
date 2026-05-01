84e62ae03015c38a9d2bba9465b361784abe54dd
rakha <dwiprayogarakha@gmail.com>
2026-05-01 20:49:23 +0700

    feat: align donor routes with gateway and update request seeder

--- services/donor-service/routes/api.php
+++ services/donor-service/routes/api.php
@@ -7,18 +7,18 @@ use App\Http\Controllers\Api\DonationScheduleController;
 use App\Http\Controllers\Api\DonationHistoryController;
 
 Route::prefix('v1')->group(function () {
-    // Publik
     Route::get('blood-types', [BloodTypeController::class, 'index']);
-    Route::get('donors', [DonorController::class, 'index']);
-    Route::get('donors/{donor}', [DonorController::class, 'show']);
-    Route::get('schedules', [DonationScheduleController::class, 'indexAll']); // tambahan
+    Route::get('schedules', [DonationScheduleController::class, 'indexAll']);
 
-    // Protected
     Route::middleware('check.user.id')->group(function () {
+        Route::get('donors', [DonorController::class, 'index']);
+        Route::get('donors/{donor}', [DonorController::class, 'show']);
+        
         Route::post('donors', [DonorController::class, 'store']);
         Route::put('donors/{donor}', [DonorController::class, 'update']);
         Route::patch('donors/{donor}', [DonorController::class, 'update']);
         Route::delete('donors/{donor}', [DonorController::class, 'destroy']);
+        
         Route::apiResource('donors.schedules', DonationScheduleController::class);
         Route::apiResource('donors.histories', DonationHistoryController::class);
     });
--- services/request-service/seed.js
+++ services/request-service/seed.js
@@ -3,9 +3,16 @@ const db = require('./config/db');
 const seedHospitals = () => {
     return new Promise((resolve, reject) => {
         const hospitals = [
-            ['RS Fatmawati', 'Jl. RS. Fatmawati Raya', '0217660552'],
-            ['RS Pondok Indah', 'Jl. Metro Pondok Indah', '0217657525'],
-            ['RS Siloam', 'Jl. Garnisun Dalam No.2-3', '02129959000']
+            ['RS Fatmawati', 'Jl. RS. Fatmawati Raya No.4, Cilandak, Jakarta Selatan', '0217660552'],
+            ['RS Pondok Indah', 'Jl. Metro Pondok Indah Kav. IV/2, Jakarta Selatan', '0217657525'],
+            ['RS Siloam Semanggi', 'Jl. Garnisun Dalam No.2-3, Jakarta Selatan', '02129959000'],
+            ['RSUPN Dr. Cipto Mangunkusumo (RSCM)', 'Jl. Diponegoro No.71, Senen, Jakarta Pusat', '0211500135'],
+            ['RS Gatot Soebroto', 'Jl. Dr. Abdul Rahman Saleh No.24, Jakarta Pusat', '0213441008'],
+            ['RS Medistra', 'Jl. Jend. Gatot Subroto Kav. 59, Jakarta Selatan', '0215210200'],
+            ['RS Tarakan', 'Jl. Kyai Caringin No.7, Gambir, Jakarta Pusat', '0213503003'],
+            ['RS Premier Bintaro', 'Jl. MH. Thamrin Blok B3 No.1, Tangerang Selatan', '02127625500'],
+            ['RS Pelni', 'Jl. Ks. Tubun No.92 - 94, Palmerah, Jakarta Barat', '0215306901'],
+            ['RS Pantai Indah Kapuk', 'Jl. Pantai Indah Utara 3, Penjaringan, Jakarta Utara', '0215880911']
         ];
         
         const query = 'INSERT IGNORE INTO hospitals (name, address, phone) VALUES ?';
